b7bc05743f0544f6c0099b49d8ffae782b441813
Olena Horovenko <elena.gorovenko@gmail.com>
2024-05-16 15:15:00 +0200

    uloha 3 with e.target

--- src/components/Uloha3/index.jsx
+++ src/components/Uloha3/index.jsx
@@ -10,8 +10,8 @@ Zadání: Doplňte komponentu tak, aby každé tlačítko po kliknutí vypsalo, 
 
 export const Uloha3 = () => {
   
-  const logChoice = (season) => {
-    console.log(`Uživatel zvolil ${season}`)
+  const logChoice = (e) => {
+    console.log(`Uživatel zvolil ${e.target.innerText}`)
   };
 
   return (
@@ -19,25 +19,25 @@ export const Uloha3 = () => {
       Které roční období je ve střední Evropě nejteplejší?
       <ol>
         <li>
-          <button onClick={() => logChoice('jaro')}>
+          <button onClick={logChoice}>
             <img src={kvetinaUrl} width={16} height={16} alt="květina" />
             jaro (březen až květen)
           </button>
         </li>
         <li>
-          <button>
+          <button onClick={logChoice}>
             <img src={slunceUrl} width={16} height={16} alt="slunce" />
             léto (červen až srpen)
           </button>
         </li>
         <li>
-          <button>
+          <button onClick={logChoice}>
             <img src={listUrl} width={16} height={16} alt="list" />
             podzim (září až listopad)
           </button>
         </li>
         <li>
-          <button>
+          <button onClick={logChoice}>
             <img src={vlockaUrl} width={16} height={16} alt="vločka" />
             zima (prosinec až únor)
           </button>
